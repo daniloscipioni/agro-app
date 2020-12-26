cdccdae7e23b6814c98427f060c1ac9906447927
Danilo Scipioni <daniloedu@gmail.com>
2020-12-26 14:55:17 -0300

    Inclusao de campos no historico acumulado

--- lib/screens/lista/maquina.dart
+++ lib/screens/lista/maquina.dart
@@ -126,18 +126,18 @@ class _machineItem extends StatelessWidget {
         child: Container(
           padding: const EdgeInsets.symmetric(vertical: 10.0),
           child: SizedBox(
-            height: 80,
+           // height: 210,
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Expanded(
                   child: Container(
-                    padding: const EdgeInsets.fromLTRB(20.0, 0.0, 2.0, 0.0),
+                   // padding: const EdgeInsets.fromLTRB(5.0, 0.0, 2.0, 0.0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
                         Padding(
-                          padding: const EdgeInsets.fromLTRB(0, 0, 0, 8),
+                          padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: <Widget>[
@@ -147,6 +147,68 @@ class _machineItem extends StatelessWidget {
                                 style: const TextStyle(
                                   fontWeight: FontWeight.bold,
                                 ),
+                              ),
+                              ListTile(
+                                dense:true,
+                                visualDensity: VisualDensity(horizontal: 0, vertical: -4),
+                                contentPadding: EdgeInsets.only(left: 0.0, right: 0.0),
+                                leading: Icon(Icons.local_gas_station,size: 18.0,),
+                                title: Align(
+                                  child: new Text('Último Abastecimento'),
+                                  alignment: Alignment.center,
+                                ),
+                                //subtitle: Text('subtitulo'),
+                                trailing: Text('305.65 Lts'),
+                                //isThreeLine: true,
+
+
+                              ),
+
+                              ListTile(
+                                dense:true,
+                                visualDensity: VisualDensity(horizontal: 0, vertical: -4),
+                                contentPadding: EdgeInsets.only(left: 0.0, right: 0.0),
+                                leading: Icon(Icons.access_time,size: 18.0),
+                                title: Align(
+                                  child: new Text('Horímetro'),
+                                  alignment: Alignment.center,
+                                ),
+                                //subtitle: Text('subtitulo'),
+                                trailing: Text('4.500h'),
+                                //isThreeLine: true,
+
+
+                              ),
+                              ListTile(
+                                dense:true,
+                                visualDensity: VisualDensity(horizontal: 0, vertical: -4),
+                                contentPadding: const EdgeInsets.fromLTRB(2.0, 0.0, 2.0, 0.0),
+                                leading: Icon(Icons.access_time,size: 18.0),
+                                title: Align(
+                                  child: new Text('Consumo Médio'),
+                                  alignment: Alignment.center,
+                                ),
+                                //subtitle: Text('subtitulo'),
+                                trailing: Text('5,6 L/h'),
+                                //isThreeLine: true,
+
+
+
+                              ),
+
+                              ListTile(
+                                dense:true,
+                                contentPadding: const EdgeInsets.fromLTRB(2.0, 0.0, 2.0, 0.0),
+                                leading: Icon(Icons.monetization_on,size: 18.0,),
+                                title: Align(
+                                  child: new Text('Custo Horário'),
+                                  alignment: Alignment.center,
+                                ),
+                                //subtitle: Text('subtitulo'),
+                                trailing: Text('R\$\ 150,00 /h'),
+                                //isThreeLine: true,
+
+                                visualDensity: VisualDensity(horizontal: 0, vertical: -4),
                               ),
                             ],
                           ),
